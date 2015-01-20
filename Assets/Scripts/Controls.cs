62862e9034b7958edb0471455c2f972e4b22c25a
unknown <kwieber@CND4220H6Z.campus.ltu.edu>
2015-01-19 19:33:55 -0500

    combining what i've done already, mostly pushing so I can get everything on my desktop

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public float movespeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.name == "P1"){
+			if(Input.GetKey("w"))
+				gameObject.transform.position+= Vector3.forward*movespeed;
+			if(Input.GetKey("a"))
+				gameObject.transform.position+= Vector3.left*movespeed;
+			if(Input.GetKey("s"))
+				gameObject.transform.position+= Vector3.back*movespeed;
+			if(Input.GetKey("d"))
+				gameObject.transform.position+= Vector3.right*movespeed;
+		}
+	
+	}
+}
