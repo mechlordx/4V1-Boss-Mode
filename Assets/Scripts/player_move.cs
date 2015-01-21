1e4b7f19917992e2b825bb48b741b9a80622914d
mechlordx <sohlrich@ltu.edu>
2015-01-22 06:18:26 +0900

    Joystick movement done in player arena

--- Assets/Scripts/player_move.cs
+++ Assets/Scripts/player_move.cs
@@ -5,18 +5,33 @@ public class player_move : MonoBehaviour {
 
 	public int playerNumber = 0;
 	public float maxspeed = 10f;
+	public float maxforce = 20f;
 	public float turnspeed = 30f;
 	public float turnbuffer = 15f;
 	float desiredangle = 0f;
+	float deadzone = .25f;
+	float hor = 0f;
+	float ver = 0f;
 	player_controls controlsRef;
+
+	// Must make so that you can still add force in the opposite direction when 
+	// over the maxspeed
+	// Make different forces for different level of maxspeed
+
 	// Use this for initialization
 	void Awake () {
 		controlsRef = GameObject.Find ("GameController").GetComponent<player_controls>();
 	}
 
 	void Update() {
-		float hor = controlsRef.getAnyAxis (playerNumber, false);
-		float ver = controlsRef.getAnyAxis (playerNumber, true);
+		hor = controlsRef.getAnyAxis (playerNumber, false);
+		ver = controlsRef.getAnyAxis (playerNumber, true);
+		if(Vector3.Magnitude(new Vector3(hor, 0f, ver))>1f)
+		{
+			float mag = Vector3.Magnitude(new Vector3(hor, 0f, ver));
+			hor = hor * (1f / mag);
+			ver = ver * (1f / mag);
+		}
 		if(hor == 0f & ver == 0f)
 			desiredangle = -1f;
 		else
@@ -28,9 +43,9 @@ public class player_move : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+		float diff = Mathf.Abs (Mathf.DeltaAngle(transform.localEulerAngles.y, desiredangle));
 		if(desiredangle!=-1)
 		{
-			float diff = Mathf.Abs (Mathf.DeltaAngle(transform.localEulerAngles.y, desiredangle));
 			if(diff<turnbuffer)
 				transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
 				                                         desiredangle,
@@ -49,5 +64,17 @@ public class player_move : MonoBehaviour {
 					                                         transform.localEulerAngles.z);
 			}
 		}
+		if(Vector3.Magnitude(rigidbody.velocity)<maxspeed && Vector3.Magnitude(new Vector3(hor, 0f, ver)) > deadzone)
+		{
+			float maxpower = 0f;
+			if(diff<90f)
+				maxpower += (maxforce * Mathf.Cos(diff * Mathf.Deg2Rad));
+			float wantedpower = ((Vector3.Magnitude(new Vector3(hor, 0f, ver))-deadzone)/(1f-deadzone)) * maxforce;
+			if(wantedpower>maxpower)
+				wantedpower = maxpower;
+
+			rigidbody.AddForce(wantedpower * new Vector3(ver, 0f, hor));
+		}
+		Debug.Log (Vector3.Magnitude (rigidbody.velocity) > maxspeed);
 	}
 }
