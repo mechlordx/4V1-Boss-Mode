8e1c6dffa6ce2bbc56a25cedc764d7e8f6bab1d7
mechlordx <sohlrich@ltu.edu>
2015-03-01 07:05:19 +0900

    a

---
+++ Assets/player_powerups.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_powerups : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
