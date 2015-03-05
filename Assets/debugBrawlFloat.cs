5ef38dc36bc2e69d1db102c6402bc2bd27f7e0c1
mechlordx <sohlrich@ltu.edu>
2015-03-06 08:05:00 +0900

    forgot to push these

---
+++ Assets/debugBrawlFloat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class debugBrawlFloat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0f, Time.deltaTime * 7f, 0f);
+		if(transform.position.y>50f)
+			Destroy(gameObject, 0f);
+	}
+}
