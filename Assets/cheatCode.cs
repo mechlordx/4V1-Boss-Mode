3e9c051cb63851db7b4961c1abdc2403b329b641
mechlordx <sohlrich@ltu.edu>
2015-03-17 07:19:11 +0900

    cheat code

---
+++ Assets/cheatCode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cheatCode : MonoBehaviour {
+
+	public string code = "a";
+	string currentCode = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.Escape))
+			currentCode = "";
+		currentCode += Input.inputString;
+		if(currentCode==code)
+			GetComponent<loader>().loadScene(1);
+	}
+}
