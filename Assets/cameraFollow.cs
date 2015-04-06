e86f3f1e5f90e732c491f6fd5eb3671499382eb5
mechlordx <sohlrich@ltu.edu>
2015-04-07 07:24:50 +0900

    Camera facing forward

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -6,14 +6,20 @@ public class cameraFollow : MonoBehaviour {
 
 	public GameObject theCockpit;
 	public GameObject playerFollow;
+	public int playerNumber;
 	GameObject theBoss;
 	Vector3 bossPosition;
 	float dis = 3f;
+	int mode = 0;
+	player_controls controlsRef;
+	Vector3 desiredRelPos;
+	Vector3 fakeBossPosition;
+
 
 	// Use this for initialization
 	void Awake () {
 		CP = theCockpit.GetComponent<newcockpit> ();
-
+		controlsRef = GameObject.Find ("GameController").GetComponent<player_controls> ();
 		theBoss = GameObject.Find ("Boss");
 		bossPosition = new Vector3(theBoss.transform.position.x,
 		                           0f,
@@ -27,19 +33,35 @@ public class cameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(controlsRef.getButton(playerNumber, 3, 1))
+		{
+			setPositionBasedOn(playerFollow.transform.position + playerFollow.transform.forward*10f);
+			desiredRelPos = transform.position - playerFollow.transform.position;
+			mode = 1;
+		}
+		if(controlsRef.getButton(playerNumber, 2, 1))
+		{
+			if(mode==0)
+			{
+				mode = 1;
+				desiredRelPos = transform.position - playerFollow.transform.position;
+			}
+			else if(mode==1)
+				mode = 0;
+		}
 		//If not the boss follow the player, if is boss become a child to the playerfor ease of following
 		if(CP.attachedPlayer == null || CP.attachedPlayer != playerFollow){
 			if(CP.attachedPlayer == null ||(playerFollow.transform.position.x != CP.attachedPlayer.transform.position.x && playerFollow.transform.position.z != CP.attachedPlayer.transform.position.z)){
 				if(transform.parent != null)
 					transform.parent = null;
-
-				transform.position = bossPosition + new Vector3(0f,playerFollow.transform.position.y,0f);
-				transform.LookAt (playerFollow.transform.position);
-				transform.position += transform.forward * (Vector3.Magnitude(transform.position - playerFollow.transform.position) + dis);
-				transform.Rotate (new Vector3 (0f, 180f, 0f));
-				transform.position += new Vector3 (0f, 1.5f, 0f);
-				transform.Rotate (new Vector3 (20f, 0f, 0f));
+				if(mode==0)
+				{
+					setPositionBasedOn(bossPosition);
+				}
+				else if(mode==1)
+				{
+					transform.position = playerFollow.transform.position + desiredRelPos;
+				}
 			}
 			else{
 				transform.position = new Vector3(0,15,-10);
@@ -47,7 +69,6 @@ public class cameraFollow : MonoBehaviour {
 			}
 
 		}
-		/**/
 		else{
 
 			if(transform.parent == null){
@@ -74,5 +95,15 @@ public class cameraFollow : MonoBehaviour {
 				
 		}
 		/**/
+	}
+
+	void setPositionBasedOn(Vector3 pos)
+	{
+		transform.position = pos + new Vector3(0f,playerFollow.transform.position.y,0f);
+		transform.LookAt (playerFollow.transform.position);
+		transform.position += transform.forward * (Vector3.Magnitude(transform.position - playerFollow.transform.position) + dis);
+		transform.Rotate (new Vector3 (0f, 180f, 0f));
+		transform.position += new Vector3 (0f, 1.5f, 0f);
+		transform.Rotate (new Vector3 (20f, 0f, 0f));
 	}
 }
