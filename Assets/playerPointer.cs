3be6fedaaaf7915690072a1f20fe9d22402b7c56
mechlordx <sohlrich@ltu.edu>
2015-04-09 05:05:21 +0900

    push2

--- Assets/playerPointer.cs
+++ Assets/playerPointer.cs
@@ -6,14 +6,19 @@ public class playerPointer : MonoBehaviour {
 	public GameObject otherPlayer;
 	float far = 10f;
 	float close = 5f;
+	int myPlayer = -1;
 
 	// Use this for initialization
 	void Awake () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(myPlayer==-1)
+		{
+			myPlayer = transform.parent.gameObject.GetComponent<player_move>().playerNumber;
+		}
+
 		transform.localPosition = Vector3.zero;
 		float dis = Vector3.Distance (otherPlayer.transform.position, transform.position);
 		float newAlpha = 0f;
@@ -22,6 +27,12 @@ public class playerPointer : MonoBehaviour {
 			newAlpha = 1f;
 		else if(dis<far)
 			newAlpha = 1f - ((dis - close) / (far - close));
+		GameObject bossPlayer = GameObject.Find ("Cockpit").GetComponent<newcockpit> ().attachedPlayer;
+		if(bossPlayer!=null)
+		{
+			if(bossPlayer.GetComponent<player_move>().playerNumber==myPlayer)
+				newAlpha = 0f;
+		}
 		Debug.Log (newAlpha);
 		Renderer[] children = GetComponentsInChildren<Renderer>();
 		foreach(Renderer child in children)
