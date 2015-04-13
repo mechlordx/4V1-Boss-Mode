74bfc388692daacf64ec78bfce8a33e14080736a
mechlordx <sohlrich@ltu.edu>
2015-04-14 07:26:02 +0900

    3 pick ups have models

---
+++ Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public Vector3 toRotate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (toRotate, Space.World);
+	}
+}
